4650256302c0fe051749999a4c1cb269cc3f31ff
Andy Jakubowski <hello@andyjakubowski.com>
2020-11-13 14:13:24 +0100

    Only show colors that haven’t been labeled yet

--- dist/javascript.js
+++ dist/javascript.js
@@ -81,7 +81,12 @@ const App = (function buildApp() {
   }
 
   function setNewColor() {
-    currentColor = ColorUtil.getRandomRGBColor();
+    const labeledColors = Data.get().index;
+
+    do {
+      currentColor = ColorUtil.getRandomRGBColor();
+      hexColor = ColorUtil.RGBToHex(currentColor);
+    } while (labeledColors.includes(hexColor));
   }
 
   function displayNewColor() {
@@ -145,24 +150,6 @@ const ColorUtil = (function buildColorUtil() {
     },
   };
 })();
-
-function appendDataDownloadLink() {
-  const sampleObject = {
-    name: 'Andy',
-    age: 102,
-  };
-  const jsonSampleObject = JSON.stringify(sampleObject);
-  const blob = new Blob([jsonSampleObject], { type: 'application/json' });
-  const url = URL.createObjectURL(blob);
-
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = 'labeled-colors.json';
-  a.textContent = 'Download labeled-colors.json';
-
-  const mainContentEl = document.getElementsByClassName('main-content').item(0);
-  mainContentEl.append(a);
-}
 
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
