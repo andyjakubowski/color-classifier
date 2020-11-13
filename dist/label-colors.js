f9a8721e60ae26706e4541494f7e766dc10c7883
Andy Jakubowski <hello@andyjakubowski.com>
2020-11-13 14:54:52 +0100

    Split JS into separate files

--- dist/label-colors.js
+++ dist/label-colors.js
@@ -123,45 +123,6 @@ const App = (function buildApp() {
   };
 })();
 
-const MathUtil = {
-  getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  },
-};
-
-const ColorUtil = (function buildColorUtil() {
-  function intToHex(int) {
-    return int.toString(16).padStart(2, '0');
-  }
-
-  return {
-    getRandomRGBColor() {
-      const red = MathUtil.getRandomInt(255);
-      const green = MathUtil.getRandomInt(255);
-      const blue = MathUtil.getRandomInt(255);
-
-      return {
-        red,
-        green,
-        blue,
-      };
-    },
-
-    getRandomHexColor() {
-      const rgbColor = this.getRandomRGBColor();
-      return this.RGBToHex(rgbColor);
-    },
-
-    RGBToHex({ red, green, blue }) {
-      const redHex = intToHex(red);
-      const greenHex = intToHex(green);
-      const blueHex = intToHex(blue);
-
-      return `#${redHex}${greenHex}${blueHex}`;
-    },
-  };
-})();
-
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
 } else {
