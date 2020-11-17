28ecac610b233a55a5f68c12069a6f23cf0d4ea6
Andy Jakubowski <hello@andyjakubowski.com>
2020-11-17 19:07:31 +0100

    Updates

--- classifier.js
+++ classifier.js
@@ -11,11 +11,30 @@ const FakeModel = (function buildFakeModel() {
   };
 })();
 
+const Model = (function buildFakeModel() {
+  let model;
+
+  return {
+    async init() {
+      model = await tf.loadLayersModel('./model/model.json');
+    },
+
+    getModel() {
+      return model;
+    },
+
+    async predict({ red, green, blue }) {
+      const x = tf.tensor2d([red, green, blue], [1, 3]);
+      const predictionTensor = model.predict(x);
+      const probabilities = await predictionTensor.data();
+      const maxProba = Math.max(...probabilities);
+      const indexOfMaxProba = probabilities.indexOf(maxProba);
+      return { probabilities, indexOfMaxProba };
+    },
+  };
+})();
+
 const App = (function buildApp() {
-  const colorInputEl = document.getElementsByClassName('color-input').item(0);
-  const predictionLabelEl = document
-    .getElementsByClassName('prediction-label')
-    .item(0);
   const predictionLabels = [
     'Red-ish',
     'Green-ish',
@@ -28,30 +47,103 @@ const App = (function buildApp() {
     'Gray-ish',
   ];
 
-  function predict() {
-    const hexColor = colorInputEl.value;
-    const predictionIndex = FakeModel.predict(hexColor);
-    const predictionLabel = predictionLabels[predictionIndex];
-    predictionLabelEl.textContent = predictionLabel;
+  let colorInputEl;
+  let barRects;
+  let predictionLabelEl;
+  let titleEl;
+
+  function getTextColorForBackground({ red, green, blue }) {
+    const THRESHOLD = 110;
+    const channels = [red, green, blue];
+    const countBelowThreshold = channels.filter((channel) => {
+      return channel < THRESHOLD;
+    }).length;
+
+    if (countBelowThreshold >= 1) {
+      return 'white';
+    } else {
+      return 'black';
+    }
   }
 
+  async function predict() {
+    const hexColor = colorInputEl.value;
+    const rgbColor = ColorUtil.hexToRgb(hexColor);
+    const { probabilities, indexOfMaxProba } = await Model.predict(rgbColor);
+
+    barRects.forEach((rect, index) => {
+      rect.style.backgroundColor = hexColor;
+      rect.style.height = `${probabilities[index] * 100}%`;
+    });
+
+    const predictionLabel = predictionLabels[indexOfMaxProba];
+    predictionLabelEl.textContent = predictionLabel;
+    predictionLabelEl.style.color = getTextColorForBackground(rgbColor);
+  }
+
+  const updateBodyBackground = (function makeUpdateBodyBackground() {
+    const BG_OPACITY = 0.25;
+    const BG_OPACITY_RGB_RANGE = Math.floor(BG_OPACITY * 255);
+    const BG_OPACITY_HEX = ColorUtil.intToHex(BG_OPACITY_RGB_RANGE);
+
+    return function (hexColor) {
+      document.body.style.backgroundColor = `${hexColor}${BG_OPACITY_HEX}`;
+    };
+  })();
+
+  const updateTitleColor = (function makeUpdateTitleColor() {
+    // const TITLEOPACITY = 0.25;
+    // const BG_OPACITY_RGB_RANGE = Math.floor(BG_OPACITY * 255);
+    // const BG_OPACITY_HEX = ColorUtil.intToHex(BG_OPACITY_RGB_RANGE);
+    const SATURATION_MAX_PERCENT = 30;
+    const LIGHTNESS_MAX_PERCENT = 30;
+
+    return function (hexColor) {
+      const { h, s, l } = ColorUtil.hexToHsl(hexColor);
+      const saturation = Math.min(s, SATURATION_MAX_PERCENT);
+      const lightness = Math.min(l, LIGHTNESS_MAX_PERCENT);
+      const propValue = `hsl(${h}, ${saturation}%, ${lightness}%)`;
+      titleEl.style.color = propValue;
+      // return "hsl(" + h + "," + s + "%," + l + "%)";
+    };
+  })();
+
   function handleColorInput() {
+    const hexColor = colorInputEl.value;
+    updateBodyBackground(hexColor);
+    updateTitleColor(hexColor);
     predict();
   }
 
   function setColorInputToRandomValue() {
-    const randomHexColor = ColorUtil.getRandomHexColor();
-    colorInputEl.value = randomHexColor;
+    const hexColor = ColorUtil.getRandomHexColor();
+    colorInputEl.value = hexColor;
+    updateBodyBackground(hexColor);
+    updateTitleColor(hexColor);
   }
 
   function addEventListeners() {
     colorInputEl.addEventListener('input', handleColorInput);
   }
 
+  function setDomReferences() {
+    const barRectsHTMLCollection = document.getElementsByClassName(
+      'bar__rect-fill'
+    );
+    barRects = [...barRectsHTMLCollection];
+    colorInputEl = document.getElementsByClassName('color-input').item(0);
+    titleEl = document.getElementsByClassName('title').item(0);
+    predictionLabelEl = document
+      .getElementsByClassName('prediction-label')
+      .item(0);
+  }
+
   return {
-    init() {
+    async init() {
+      setDomReferences();
       addEventListeners();
       setColorInputToRandomValue();
+      await Model.init();
       predict();
     },
   };
--- util.js
+++ util.js
@@ -5,11 +5,11 @@ const MathUtil = {
 };
 
 const ColorUtil = (function buildColorUtil() {
-  function intToHex(int) {
-    return int.toString(16).padStart(2, '0');
-  }
-
   return {
+    intToHex(int) {
+      return int.toString(16).padStart(2, '0');
+    },
+
     getRandomRGBColor() {
       const red = MathUtil.getRandomInt(255);
       const green = MathUtil.getRandomInt(255);
@@ -24,15 +24,87 @@ const ColorUtil = (function buildColorUtil() {
 
     getRandomHexColor() {
       const rgbColor = this.getRandomRGBColor();
-      return this.RGBToHex(rgbColor);
+      return this.rgbToHex(rgbColor);
     },
 
-    RGBToHex({ red, green, blue }) {
-      const redHex = intToHex(red);
-      const greenHex = intToHex(green);
-      const blueHex = intToHex(blue);
+    rgbToHex({ red, green, blue }) {
+      const redHex = this.intToHex(red);
+      const greenHex = this.intToHex(green);
+      const blueHex = this.intToHex(blue);
 
       return `#${redHex}${greenHex}${blueHex}`;
+    },
+
+    hexToRgb(hexColor) {
+      let hexValue;
+
+      if (hexColor.startsWith('#')) {
+        hexValue = hexColor.slice(1);
+      } else {
+        hexValue = hexColor;
+      }
+
+      const redHexStr = hexValue.slice(0, 2);
+      const greenHexStr = hexValue.slice(2, 4);
+      const blueHexStr = hexValue.slice(4, 6);
+
+      const redInt = Number.parseInt(redHexStr, 16);
+      const greenInt = Number.parseInt(greenHexStr, 16);
+      const blueInt = Number.parseInt(blueHexStr, 16);
+
+      return { red: redInt, green: greenInt, blue: blueInt };
+    },
+
+    hexToHsl(hexColor) {
+      const rgbColor = this.hexToRgb(hexColor);
+      return this.rgbToHsl(rgbColor);
+    },
+
+    rgbToHsl({ red: r, green: g, blue: b }) {
+      // This function taken from this CSS Tricks article:
+      // https://css-tricks.com/converting-color-spaces-in-javascript/
+
+      // Make r, g, and b fractions of 1
+      r /= 255;
+      g /= 255;
+      b /= 255;
+
+      // Find greatest and smallest channel values
+      let cmin = Math.min(r, g, b),
+        cmax = Math.max(r, g, b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+
+      // Calculate hue
+      // No difference
+      if (delta == 0) h = 0;
+      // Red is max
+      else if (cmax == r) h = ((g - b) / delta) % 6;
+      // Green is max
+      else if (cmax == g) h = (b - r) / delta + 2;
+      // Blue is max
+      else h = (r - g) / delta + 4;
+
+      h = Math.round(h * 60);
+
+      // Make negative hues positive behind 360°
+      if (h < 0) {
+        h += 360;
+      }
+
+      // Calculate lightness
+      l = (cmax + cmin) / 2;
+
+      // Calculate saturation
+      s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+      // Multiply l and s by 100
+      s = +(s * 100).toFixed(1);
+      l = +(l * 100).toFixed(1);
+
+      return { h, s, l };
     },
   };
 })();
